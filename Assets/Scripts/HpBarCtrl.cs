cdf3e31de46162485ba68d78fd88b5d969e4052d
Iekiti Opelia <opelia@example.com>
2016-05-07 01:28:26 +0900

    hpバーつけましたよ
    プレイ押すと０から１００まで伸びていきます。
    
    それ以外は何もないです。

---
+++ Assets/Scripts/HpBarCtrl.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // ←※これを忘れずに入れる
+
+public class HpBarCtrl : MonoBehaviour
+{
+
+    Slider _slider;
+    public UnityEngine.UI.Text hp;
+    void Start()
+    {
+        // スライダーを取得する
+        _slider = GameObject.Find("Slider").GetComponent<Slider>();
+    }
+
+    float _hp = 0;
+    void Update()
+    {
+        // HP上昇
+        if (_hp < _slider.maxValue)
+        {
+        _hp +=1f;
+        // HPゲージに値を設定
+        _slider.value = _hp;
+        hp.text = _hp + "/100";
+        }
+
+     
+    }
+}
