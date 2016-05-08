19643b3d396454fd0fa0ff0dfe9d781c5ac3dd08
Iekiti Opelia <opelia@example.com>
2016-05-08 22:31:44 +0900

    ダメージのインスタンス追加しておきましたよ
    追加点
    public static bool kirikae = false;　//hpをダメージから減った時に再度回復しないための変数
    public static float dmg; //　ダメージの値をここに格納
    インスタンスの追加
    public static void Damage (float Damage)
    {
    dmg = Damage;
    }
    
    問題起きたら連絡ください

--- Assets/Scripts/HpBarCtrl.cs
+++ Assets/Scripts/HpBarCtrl.cs
@@ -7,24 +7,38 @@ public class HpBarCtrl : MonoBehaviour
 
     Slider _slider;
     public UnityEngine.UI.Text hp;
+
     void Start()
     {
         // スライダーを取得する
         _slider = GameObject.Find("Slider").GetComponent<Slider>();
     }
 
+    public static bool kirikae = false;
+    public static float dmg;
     float _hp = 0;
     void Update()
     {
         // HP上昇
-        if (_hp < _slider.maxValue)
+        if (_hp < _slider.maxValue&&kirikae==false)
         {
         _hp +=1f;
         // HPゲージに値を設定
         _slider.value = _hp;
         hp.text = _hp + "/100";
+             if (_hp == _slider.maxValue)
+                 { kirikae = true; 
+        }
+             
         }
 
-     
+        _hp = _hp - dmg;
+        hp.text = _hp + "/100";
     }
+
+    public static void Damage (float Damage)
+    {
+        dmg = Damage;
+    }
+
 }
