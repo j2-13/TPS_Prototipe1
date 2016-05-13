90478e4e75eafccaddfb7f6047d4bbcf7325ef44
Reverie-Atlasfiel-Leonheart <kznrh74@gmail.com>
2016-05-14 01:43:33 +0900

    5月14日ユニティちゃん移動処理更新
    移動処理更新しました。

--- Assets/Scripts/UnityChanController.cs
+++ Assets/Scripts/UnityChanController.cs
@@ -6,17 +6,22 @@ public class UnityChanController : MonoBehaviour
 {
 	
 	private Animator animator;
+	private float Yrotation;
+	private float nextjump;
 	private int doWalkId;
 	private int doRunId;
 	private int doBackWalkId;
-
+	private bool jump;
+	private float jumpRate = 3;
 	// Use this for initialization
 	void Start () 
 	{
 		animator = GetComponent<Animator> ();
 		doWalkId = Animator.StringToHash ("Do Walk");
 		doRunId = Animator.StringToHash ("Do Run");
-		doBackWalkId = Animator.StringToHash ("Do BackWalk");	
+		doBackWalkId = Animator.StringToHash ("Do BackWalk");
+
+		jump = false;
 	}
 	
 	// Update is called once per frame
@@ -52,6 +57,41 @@ public class UnityChanController : MonoBehaviour
 			animator.SetBool(doBackWalkId, false);
 		}
 
+		if(Input.GetKey(KeyCode.A))
+		{
+			Yrotation+=70*Time.deltaTime;
+			transform.rotation=Quaternion.Euler(0,Yrotation,0);
+		}
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			Yrotation-=70*Time.deltaTime;
+			transform.rotation=Quaternion.Euler(0,Yrotation,0);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)&& (Time.time > nextjump ))
+		{
+			nextjump = Time.time + jumpRate; //3秒足す) 		
+			jump = true;
+			animator.SetBool("Jumping", jump);
+			Invoke ("Jumping",2);
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space) && Input.GetKey(KeyCode.W) && (Time.time > nextjump ))
+		{
+			nextjump = Time.time + jumpRate; //3秒足す) 		
+			jump = true;
+			animator.SetBool("Jumping", jump);
+			Invoke ("Jumping",2);
+		}
 	}
 
+	private void Jumping()
+	{
+		if (jump == true)
+		{
+			jump = false;
+			animator.SetBool("Jumping", jump);
+		}
+	}
 }
